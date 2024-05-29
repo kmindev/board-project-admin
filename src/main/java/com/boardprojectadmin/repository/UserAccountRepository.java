41280e2668c70c4a7e14de1d90798bfaac681455
kmindev <kangkm0324@gmail.com>
2024-05-29 21:06:35 +0900

    #40 - 어드민 회원 테이블명 교체
    `user_account` -> `admin_account`로 어드민 회원 테이블명 변경.
    통신하려는 게시판 서비스의 회원 도메인명과 이름이 겹친다.

--- src/main/java/com/boardprojectadmin/repository/UserAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.boardprojectadmin.repository;
-
-import com.boardprojectadmin.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
-
-}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
-insert into user_account (user_id, user_password, role_types, nickname, email, memo, created_at, created_by, modified_at, modified_by)
+insert into admin_account (user_id, user_password, role_types, nickname, email, memo, created_at, created_by, modified_at, modified_by)
 values ('kkm', '{noop}asdf1234', 'ADMIN', 'kkm', 'kkm@mail.com', 'I am kkm.', now(), 'kkm', now(), 'kkm'),
        ('mark', '{noop}asdf1234', 'MANAGER', 'Mark', 'mark@mail.com', 'I am Mark.', now(), 'kkm', now(), 'kkm'),
        ('susan', '{noop}asdf1234', 'MANAGER,DEVELOPER', 'Susan', 'Susan@mail.com', 'I am Susan.', now(), 'kkm', now(),
