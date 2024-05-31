224fa53861114ed1bbfda71e0888229890a43875
kmindev <kangkm0324@gmail.com>
2024-05-31 22:57:42 +0900

    #15 - 어드민 회원 컨트롤러에 필요한 핸들러 메소드 및 테스트

--- src/main/java/com/boardprojectadmin/controller/AdminAccountController.java
+++ src/main/java/com/boardprojectadmin/controller/AdminAccountController.java
@@ -1,12 +1,12 @@
 package com.boardprojectadmin.controller;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import com.boardprojectadmin.dto.AdminAccountResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/admin/members")
 @Controller
@@ -15,6 +15,18 @@ public class AdminAccountController {
     @GetMapping
     public String members(Model model) {
         return "admin/members";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/admin/members")
+    public List<AdminAccountResponse> getMembers() {
+        return List.of();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    @DeleteMapping("/api/admin/members/{userId}")
+    public void delete(@PathVariable String userId) {
     }
 
 }
